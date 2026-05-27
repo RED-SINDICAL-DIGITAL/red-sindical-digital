1f6b8cc2fc813cc59a7fca085f3a4ee33279afd8
beatdigitalok <153673222+beatdigitalok@users.noreply.github.com>
2026-05-27 19:31:30 -0300

    Update Sidebar.tsx

--- apps/core-system/src/components/Sidebar.tsx
+++ apps/core-system/src/components/Sidebar.tsx
@@ -1,1 +1,19 @@
+Estructura:
 
+logo arriba
+navegación vertical
+iconos
+footer institucional
+
+Items:
+
+Dashboard
+Afiliados
+Credenciales
+Inspectores
+Empresas
+Cobranzas
+Noticias
+Beneficios
+Auditoría
+Configuración
