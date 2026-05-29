3123f33a72e6c282b1bdb3f8404ae34286d0fad3
beatdigitalok <153673222+beatdigitalok@users.noreply.github.com>
2026-05-29 17:43:38 -0300

    Add Home component with MainLayout and Dashboard

---
+++ apps/core-system/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import MainLayout from "../layouts/MainLayout";
+import Dashboard from "../pages/Dashboard";
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <Dashboard />
+    </MainLayout>
+  );
+}
