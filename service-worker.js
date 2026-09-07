3a47e3c01b6f74ffdc97a45f6de2e8dd5effc3f7
beatdigitalok <153673222+beatdigitalok@users.noreply.github.com>
2026-09-07 20:52:10 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,33 @@
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
-  // Borrar cachés viejas
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))));
+// service-worker.js
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('uadavstream-v2').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/landing-uadavstream.html'
+      ]).catch(err => console.warn('Error al cachear:', err));
+    })
+  );
 });
-self.addEventListener('fetch', (e) => {
-  // No cachear NADA, siempre ir al servidor
-  return;
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== 'uadavstream-v2').map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request).catch(() => {
+        return new Response('', { status: 404 });
+      });
+    })
+  );
 });
